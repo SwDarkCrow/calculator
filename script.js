589af58fc4fff674ef113b42d2e899ca46f1a5dd
SwDarkCrow <132205783+SwDarkCrow@users.noreply.github.com>
2023-08-27 17:12:28 +0200

    Can put in numbers

--- script.js
+++ script.js
@@ -17,7 +17,27 @@ function operate(operator, a, b) {
   return result;
 }
 
-const display = document.querySelector('.display');
+let display = document.getElementById('centered-display');
 function putOnDisplay(string = '0'){
-  display.textContent = string;
+  if (display.textContent.length >= 13) return;
+  if (display.textContent == '0') {
+    display.textContent = string;
+    return;
+  }
+  display.textContent += string;
+}
+
+const digits = document.querySelectorAll('.digit');
+for (const digit of Array.from(digits)){
+  digit.addEventListener('click', () => {
+    if (digit.id === 'delete'){
+      deleteLastDigit();
+      return;
+    }
+    putOnDisplay(digit.textContent);
+  });
+}
+
+function deleteLastDigit(){
+
 }
